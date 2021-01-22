1e59e1e91ca4a653397c8661eab57fbe550deaa1
Joao Paulo Figueira <cliff_haunted@hotmail.com>
2021-01-22 15:18:57 -0500

    Criando operaçao de instanciação

--- src/main/java/com/cursospringudemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursospringudemy/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.cursospringudemy.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
